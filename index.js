40f5658f7ac4da1d0f6fe3ac80c886defe87071e
delusional51 <BenjaminZanelli51@gmail.com>
2023-10-05 13:12:29 -0400

    seeing if nfl season is happening !

---
+++ index.js
@@ -0,0 +1,26 @@
+const date= new Date();
+const month= date.getMonth();
+const sports = [
+
+    {sport:'Football', 
+        conference:['afc','nfc'], 
+        divisions:['afc east','afc south','afc north','afc west','nfc east','nfc south','nfc north','nfc west'], 
+        afceteams:['patriots','jets','bills','dolphins'],
+        afcsteams:['texans','colts','jaguars','titans'],
+        afcnteams:['steelers','bengals','ravens','browns'],
+        afcwteams:['chiefs','broncos','raiders','chargers'],
+        nfceteams:['eagles','giants','cowboys','commanders'],
+        nfcsteams:['saints','falcons','panthers','buccaneers'],
+        nfcnteams:['vikings','packers','lions','bears'],
+        nfcwteams:['rams','49ers','seahawks','cardinals'],
+        viewers: 16.7,
+        playing: function() {
+            return month;
+            if () {
+                
+            }
+        }
+        
+        
+}
+]

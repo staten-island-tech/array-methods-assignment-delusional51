3b32b74517f3640f447e3c69af4283f05aacb315
delusional51 <BenjaminZanelli51@gmail.com>
2023-10-10 13:17:36 -0400

    fixed

--- index.js
+++ index.js
@@ -3,25 +3,40 @@ const month= date.getMonth();
 const sports = [
     {
         name:'Football', 
-        conference:['afc','nfc'], 
-        divisions:['afc east','afc south','afc north','afc west','nfc east','nfc south','nfc north','nfc west'], 
-        afceteams:['patriots','jets','bills','dolphins'],
-        afcsteams:['texans','colts','jaguars','titans'],
-        afcnteams:['steelers','bengals','ravens','browns'],
-        afcwteams:['chiefs','broncos','raiders','chargers'],
-        nfceteams:['eagles','giants','cowboys','commanders'],
-        nfcsteams:['saints','falcons','panthers','buccaneers'],
-        nfcnteams:['vikings','packers','lions','bears'],
-        nfcwteams:['rams','49ers','seahawks','cardinals'],
+        teams: 32,
         viewers: 16.7,
+        someplayers: ['Mahomes','Allen','Hill','Tagovailoa','Donald','CMC'],
         playing: function(x) {
-            if (x = 1||2||9||10||11||12) {
+            if (x === 1||2||9||10||11||12) {
                 console.log("Playing") }
                 else { console.log("Not Playing")}
           }
- }
+     },
+     {
+        name:'Basketball',
+        teams: 30,
+        viewers: 12.4,
+        someplayers: ['Lebron','Giannas','Butler','Jokic','Embid','Lillard','Bam'],
+        playing: function(x) {
+            if (x === 1||2||3||4||5||6||10||11||12) {
+                console.log("Playing") }
+                else { console.log("Not Playing")}
+          }
+     },
+     {
+        name:'Fencing',
+        teams:44,
+        viewers:3050,
+        someplayers: ['Borel','Siklosi','Meindhart','Kiefer','Romain','Ka Long'],
+        playing: function(x) {
+            if (x === 7||8) {
+                console.log("Playing") }
+                else { console.log("Not Playing")}
+          }
+     },
 ]
 function mon() {
     return month
-  }
-console.log(sports.playing(month));    
+  }; 
+sports.forEach((sport) => console.log(sport.name));
+sports.forEach((sport) => console.log(sport.playing(month)));

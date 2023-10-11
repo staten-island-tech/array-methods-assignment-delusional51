26e30b2a6a889f28c62522191e62b56926d57004
delusional51 <BenjaminZanelli51@gmail.com>
2023-10-11 13:14:59 -0400

    YES

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
-const date= new Date();
-const month= date.getMonth();
 const sports = [
     {
         name:'Football', 
         teams: 32,
         viewers: 16.7,
-        someplayers: ['Mahomes','Allen','Hill','Tagovailoa','Donald','CMC'],
-        playing: function(x) {
-            if (x === 1||2||9||10||11||12) {
-                console.log("Playing") }
-                else { console.log("Not Playing")}
-          }
+        someplayers: ['Mahomes','Allen','Hill','Tagovailoa'],
+       
      },
      {
         name:'Basketball',
         teams: 30,
         viewers: 12.4,
-        someplayers: ['Lebron','Giannas','Butler','Jokic','Embid','Lillard','Bam'],
-        playing: function(x) {
-            if (x === 1||2||3||4||5||6||10||11||12) {
-                console.log("Playing") }
-                else { console.log("Not Playing")}
-          }
+        someplayers: ['Lebron','Giannas','Butler','Jokic'],
+       
      },
      {
         name:'Fencing',
         teams:44,
         viewers:3050,
-        someplayers: ['Borel','Siklosi','Meindhart','Kiefer','Romain','Ka Long'],
-        playing: function(x) {
-            if (x === 7||8) {
-                console.log("Playing") }
-                else { console.log("Not Playing")}
-          }
+        someplayers: ['Borel','Siklosi','Meindhart','Kiefer'],
+       
      },
 ]
-function mon() {
-    return month
-  }; 
+function fun(x){
+    console.log(x[0]);
+    console.log(x[1]);
+    console.log(x[2]);
+    console.log(x[3]);
+}
 sports.forEach((sport) => console.log(sport.name));
-sports.forEach((sport) => console.log(sport.playing(month)));
+sports.forEach((sport)=> fun(sport.someplayers));
+
